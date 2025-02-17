9330244ac2c2fa57c278e8c7d514110571582d92
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-17 14:33:18 +0000

    import testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import ServicesSection from "@/components/Services";
 import ProjectsSection from "@/components/Projects";
+import TestimonialsSection from "@/components/Testimonials";
 export default function HeroPage() {
   return (
     <div className="relative">
@@ -11,6 +12,7 @@ export default function HeroPage() {
       <AboutSection />
       <ServicesSection />
       <ProjectsSection />
+      <TestimonialsSection />
     </div>
   );
 }
