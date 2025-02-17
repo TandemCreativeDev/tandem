e20b79663d05eafe6ac2f3d458f3c21f41b40a2d
jackcasstlesjones <96030524+jackcasstlesjones@users.noreply.github.com>
2025-02-17 17:56:31 +0000

    Update src/components/Team.tsx
    Co-authored-by: Maxime Downe <81163597+maxitect@users.noreply.github.com>

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,6 +1,6 @@
 export default function TeamSection() {
   return (
-    <>
+    <section id="team">
       <div className="h-[1000px] w-full bg-black  pt-10">
         <div className="h-1/4 flex justify-between w-3/4 m-auto ">
           <h3>projects</h3>
@@ -25,6 +25,6 @@ export default function TeamSection() {
           </div>
         </div>
       </div>
-    </>
+    </section>
   );
 }
