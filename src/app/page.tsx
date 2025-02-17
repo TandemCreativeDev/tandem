3030cd99a05f550f3d2433dde737ab1ad50e64fd
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-17 10:32:02 +0000

    add services section to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutSection from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import ServicesSection from "@/components/Services";
 export default function HeroPage() {
   return (
     <div className="relative">
       <Header />
       <Hero />
       <AboutSection />
+      <ServicesSection />
     </div>
   );
 }
