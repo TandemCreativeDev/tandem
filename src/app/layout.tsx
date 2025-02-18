1d5a9ff385161dd49ee116c337b5d50855080f1f
maxitect <maxime.downe@gmail.com>
2025-02-18 18:21:47 +0000

    style: import fonts into layout.tsx and bring into page so tailwind can access

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {
+  diatypeRegular,
+  diatypeMedium,
+  diatypeMonoRegular,
+  diatypeMonoMedium,
+  diatypeCondensedMedium,
+} from "@/fonts/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={`${diatypeRegular.variable} ${diatypeMedium.variable} ${diatypeMonoRegular.variable} ${diatypeMonoMedium.variable} ${diatypeCondensedMedium.variable}`}
+    >
+      <body className="font-tandem-regular antialiased relative">{children}</body>
     </html>
   );
 }
