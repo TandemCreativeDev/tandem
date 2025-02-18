04ab67ab6129cc2cb2a7e369ca80b372c3042407
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-18 11:48:32 +0000

    header is now absolute to allow for absolute hero image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,17 @@ import Nav from "./Nav";
 import Time from "./Time";
 export default function Header() {
   return (
-    <div className=" bg-none -z-50 flex justify-between w-11/12 m-auto uppercase font-tandem-mono font-medium pt-5">
-      <div className="w-1/3">
-        <Time />
-      </div>
-      <div className="w-1/3">
-        <h1 className="text-center">tandem creative dev</h1>
-      </div>
-      <div className="w-1/3 self-end">
-        <Nav />
+    <div className=" bg-none   flex justify-between w-full m-auto uppercase font-tandem-mono font-medium pt-5 absolute top-0 left-0 z-50">
+      <div className="m-auto w-11/12 flex justify-between">
+        <div className="w-1/3">
+          <Time />
+        </div>
+        <div className="w-1/3">
+          <h1 className="text-center">tandem creative dev</h1>
+        </div>
+        <div className="w-1/3 self-end">
+          <Nav />
+        </div>
       </div>
     </div>
   );
