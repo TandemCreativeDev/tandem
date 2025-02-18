80260463af9b262a7302288893a6e9c736fd6f5d
maxitect <maxime.downe@gmail.com>
2025-02-18 18:29:50 +0000

    style: make Time.tsx look like figma design (lol still not a clock)

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,8 +1,8 @@
 export default function Time() {
   return (
     <div className="flex justify-start  gap-5">
-      <p>London</p>
-      <p>4pm</p>
+      <p>• London</p>
+      <p>4:44pm</p>
     </div>
   );
 }
