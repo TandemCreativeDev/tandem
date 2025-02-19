a3bfc68642968e7e784fb79527997a28107ea8a5
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-19 15:24:13 +0000

    team portrait animated size change

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import clsx from "clsx";
 import { useState } from "react";
 import Image from "next/image";
 
 export default function TeamSection() {
-  const [activeImage, setActiveImage] = useState(0);
-  const bigSize = 500;
-  const smallSize = 300;
+  const [activeImage, setActiveImage] = useState(100);
   return (
     <section
       id="team"
@@ -25,24 +22,50 @@ export default function TeamSection() {
         <div className="col-start-5 col-span-8">
           <div className="flex gap-10 items-end font-tandem-mono uppercase text-white">
             <div className="flex flex-col">
-              <Image
-                src={`/portraits/portrait-jack.jpg`}
-                alt="Portrait of Jack, one of the Tandem founders"
-                height={activeImage === 0 ? bigSize : smallSize}
-                width={activeImage === 0 ? bigSize : smallSize}
-                onClick={() => setActiveImage(0)}
-              ></Image>
+              <button>
+                <div
+                  className={`transition-all duration-700 ${
+                    activeImage === 0
+                      ? "w-[500px] h-[500px]"
+                      : activeImage === 1
+                        ? "w-[380px] h-[380px]"
+                        : "w-[400px] h-[400px]"
+                  }`}
+                >
+                  <Image
+                    src="/portraits/portrait-jack.jpg"
+                    alt="Portrait of Jack, one of the Tandem founders"
+                    layout="intrinsic"
+                    width={500}
+                    height={500}
+                    onMouseEnter={() => setActiveImage(0)}
+                  />
+                </div>
+              </button>
               <p>Jack Casstles-Jones</p>
               <p>Co-Founder \ Full-Stack Developer</p>
             </div>
             <div className="flex flex-col">
-              <Image
-                src={`/portraits/portrait-max.jpg`}
-                alt="Portrait of Jack, one of the Tandem founders"
-                height={activeImage === 1 ? bigSize : smallSize}
-                width={activeImage === 1 ? bigSize : smallSize}
-                onClick={() => setActiveImage(1)}
-              ></Image>
+              <button>
+                <div
+                  className={`transition-all duration-700 ${
+                    activeImage === 1
+                      ? "w-[500px] h-[500px]"
+                      : activeImage === 0
+                        ? "w-[380px] h-[380px]"
+                        : "w-[400px] h-[400px]"
+                  }`}
+                >
+                  <Image
+                    src={`/portraits/portrait-max.jpg`}
+                    alt="Portrait of Jack, one of the Tandem founders"
+                    layout="intrinsic"
+                    width={500}
+                    height={500}
+                    onMouseEnter={() => setActiveImage(1)}
+                  ></Image>
+                </div>
+              </button>
               <p>Maxime Downe</p>
               <p>Co-Founder \ Full-Stack Developer</p>
             </div>
