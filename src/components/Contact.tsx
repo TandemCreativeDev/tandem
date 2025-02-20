d552591fb72b862ed12afdf38cb4086eb87b62ed
maxitect <maxime.downe@gmail.com>
2025-02-20 14:38:26 +0000

    chore: add Contact.tsx section component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+export default function ContactSection() {
+  return (
+    <section
+      id="contact"
+      className="bg-white w-10/12 m-auto gap-10 md:gap-0 md:w-screen pt-20 md:pt-36 flex flex-col md:grid grid-cols-12 h-[400px] md:h-[500px]"
+    >
+      <h2 className="uppercase font-tandem-mono-medium text-xs col-start-3 col-span-1">
+        ■ Contact
+      </h2>
+    </section>
+  );
+}
