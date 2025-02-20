ccc048bf7aca7c5d89e386049bbd1296b15128a9
maxitect <maxime.downe@gmail.com>
2025-02-20 21:56:34 +0000

    fix: email secure boolea no use string comparison in nodemailer.ts

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: Boolean(process.env.EMAIL_SECURE),
+  secure: process.env.EMAIL_SECURE == "true",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
