6e2e2029f124d37747b12499b5c4083a65cae3e6
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-20 15:51:17 +0000

    install react icons with responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import Nav from "./Nav";
 import Time from "./Time";
+import { BsList } from "react-icons/bs";
+
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolledPastHero, setHasScrolledPastHero] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const heroHeight = window.innerHeight;
+      const scrollPosition = window.scrollY;
+      setHasScrolledPastHero(scrollPosition > heroHeight);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
@@ -12,24 +27,40 @@ export default function Header() {
       document.body.style.overflow = "auto";
     }
   }, [isOpen]);
+
   return (
-    <header className="text-white bg-none flex justify-between w-full m-auto uppercase font-tandem-mono-medium pt-5 absolute top-0 left-0 z-50">
-      <div className="m-auto w-11/12 flex justify-between">
+    <header
+      className={`
+        text-white
+        flex 
+        justify-between 
+        items-center
+        w-full 
+        m-auto 
+        uppercase 
+        font-tandem-mono-medium 
+        pt-5
+        ${hasScrolledPastHero ? "fixed top-0 left-0 bg-black pb-5" : "absolute top-0 left-0 bg-none"}
+        transition-all 
+        duration-300 
+        z-50
+      `}
+    >
+      <div className="m-auto w-11/12 flex xl:justify-between items-center justify-center">
         <div className="w-1/3 xl:block hidden">
           <Time />
         </div>
         <div className="w-full xl:w-1/3">
-          <h1 className="text-center ">tandem creative dev</h1>
+          <h1 className="text-center">tandem creative dev</h1>
         </div>
         <div className="w-1/3 self-end xl:block hidden">
           <Nav />
         </div>
-        <p
-          className="xl:hidden sticky top-10"
+        <BsList
+          size={40}
+          className="xl:hidden absolute top-3 right-5"
           onClick={() => setIsOpen(!isOpen)}
-        >
-          uhambuuuuurger
-        </p>
+        />
         {isOpen === true ? (
           <div className="absolute h-screen top-0 left-0 w-screen bg-white flex justify-center items-center text-5xl overflow-hidden">
             <p
