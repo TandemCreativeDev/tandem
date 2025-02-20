ff7df07e50b737802e45fcfd81a297651c658c38
maxitect <maxime.downe@gmail.com>
2025-02-20 17:05:58 +0000

    chore: update Contact.ts section with form components

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,209 @@
+"use client";
+
+import { useState } from "react";
+import { Field, Label, Switch } from "@headlessui/react";
+
 export default function ContactSection() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    company: "",
+    email: "",
+    phoneNumber: "",
+    country: "",
+    message: "",
+  });
+
+  const [agreed, setAgreed] = useState(false);
+
+  const handleChange = (e: { target: { name: string; value: unknown } }) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  async function handleSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+          company: formData.company,
+          email: formData.email,
+          phoneNumber: formData.phoneNumber,
+          message: formData.message,
+        }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        alert(data.message);
+      } else {
+        alert(`Something went wrong: ${data.error}`);
+      }
+    } catch (error) {
+      alert(`Failed to send message. Please try again later. ${error}`);
+    }
+  }
+
   return (
     <section
       id="contact"
-      className="bg-white w-10/12 m-auto gap-10 md:gap-0 md:w-screen pt-20 md:pt-36 flex flex-col md:grid grid-cols-12 h-[400px] md:h-[500px]"
+      className="bg-white w-10/12 m-auto gap-10 md:gap-0 md:w-screen pt-20 md:pt-36 flex flex-col md:grid grid-cols-12"
     >
       <h2 className="uppercase font-tandem-mono-medium text-xs col-start-3 col-span-1">
         ■ Contact
       </h2>
+      <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label
+              htmlFor="firstName"
+              className="block text-sm/6 font-semibold"
+            >
+              First name
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="firstName"
+                name="firstName"
+                type="text"
+                value={formData.firstName}
+                onChange={handleChange}
+                className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-viaOrange sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="lastName" className="block text-sm/6 font-semibold">
+              Last name
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="lastName"
+                name="lastName"
+                type="text"
+                value={formData.lastName}
+                onChange={handleChange}
+                className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-viaOrange sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="company" className="block text-sm/6 font-semibold">
+              Company
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="company"
+                name="company"
+                type="text"
+                value={formData.company}
+                onChange={handleChange}
+                className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-viaOrange sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="email" className="block text-sm/6 font-semibold">
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-viaOrange sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="phoneNumber"
+              className="block text-sm/6 font-semibold"
+            >
+              Phone number
+            </label>
+            <div className="relative mt-2.5">
+              <div className="absolute inset-y-0 left-2 flex items-center">
+                <label htmlFor="country" className="sr-only">
+                  Country
+                </label>
+                <select
+                  id="country"
+                  name="country"
+                  value={formData.country}
+                  onChange={handleChange}
+                  className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-8 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-viaOrange sm:text-sm"
+                >
+                  <option value="UK">UK</option>
+                  <option value="US">US</option>
+                  <option value="EU">EU</option>
+                </select>
+              </div>
+              <input
+                id="phone-number"
+                name="phoneNumber"
+                type="tel"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-24 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-viaOrange sm:text-sm"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="message" className="block text-sm/6 font-semibold">
+              Message
+            </label>
+            <div className="mt-2.5">
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                value={formData.message}
+                onChange={handleChange}
+                className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-viaOrange sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <Field className="flex gap-x-4 sm:col-span-2">
+            <div className="flex h-6 items-center">
+              <Switch
+                checked={agreed}
+                onChange={setAgreed}
+                className="group flex w-8 flex-none cursor-pointer rounded-full bg-gray-200 p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-viaOrange data-[checked]:bg-viaOrange"
+              >
+                <span className="sr-only">Agree to policies</span>
+                <span
+                  aria-hidden="true"
+                  className="h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out group-data-[checked]:translate-x-3.5"
+                />
+              </Switch>
+            </div>
+            <Label className="text-sm/6">
+              By selecting this, you agree to our{" "}
+              <a href="#" className="font-semibold text-viaOrange">
+                privacy&nbsp;policy
+              </a>
+              .
+            </Label>
+          </Field>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="z-50 block text-black w-full rounded-md bg-white px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm hover:bg-[#fd8960] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-viaOrange"
+          >
+            Let&apos;s talk
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
