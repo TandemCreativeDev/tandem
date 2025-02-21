77589e3cf920a2567823f79dc6c6b3ec20851a8b
maxitect <maxime.downe@gmail.com>
2025-02-21 18:49:32 +0000

    chore: add simple rendering of Checkbox.tsx for those prefering reduced motion

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface CheckboxProps {
@@ -19,27 +22,52 @@ export default function Checkbox({
   urlText,
   required = false,
 }: CheckboxProps) {
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+    setPrefersReducedMotion(mediaQuery.matches);
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setPrefersReducedMotion(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
   return (
     <div className="flex gap-2 pb-6 col-span-2">
-      <div className="checkbox-wrapper-30">
-        <span className="checkbox">
-          <input id={id} name={name} type="checkbox" required={required} />
-          <svg>
-            <use xlinkHref="#checkbox-30" className="checkbox"></use>
+      {prefersReducedMotion ? (
+        <input
+          id={id}
+          name={name}
+          type="checkbox"
+          required={required}
+          className="block"
+        />
+      ) : (
+        <div className="checkbox-wrapper-30">
+          <span className="checkbox">
+            <input id={id} name={name} type="checkbox" required={required} />
+            <svg>
+              <use xlinkHref="#checkbox-30" className="checkbox"></use>
+            </svg>
+          </span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="hidden">
+            <symbol id="checkbox-30" viewBox="0 0 22 22">
+              <path
+                fill="none"
+                stroke="currentColor"
+                d="M5.5,11.3L9,14.8L20.2,3.3l0,0c-0.5-1-1.5-1.8-2.7-1.8h-13c-1.7,0-3,1.3-3,3v13c0,1.7,1.3,3,3,3h13 
+                  c1.7,0,3-1.3,3-3v-13c0-0.4-0.1-0.8-0.3-1.2"
+              />
+            </symbol>
           </svg>
-        </span>
-        <svg xmlns="http://www.w3.org/2000/svg" className="hidden">
-          <symbol id="checkbox-30" viewBox="0 0 22 22">
-            <path
-              fill="none"
-              stroke="currentColor"
-              d="M5.5,11.3L9,14.8L20.2,3.3l0,0c-0.5-1-1.5-1.8-2.7-1.8h-13c-1.7,0-3,1.3-3,3v13c0,1.7,1.3,3,3,3h13 c1.7,0,3-1.3,3-3v-13c0-0.4-0.1-0.8-0.3-1.2"
-            />
-          </symbol>
-        </svg>
-      </div>
+        </div>
+      )}
       <label htmlFor={id} className={twMerge("text-sm/6", labelClass)}>
-        {label}
+        {label}{" "}
         <a target="_blank" href={url} className="font-bold text-blue-600">
           {urlText}
         </a>
