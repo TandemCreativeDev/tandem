b095770c3601b9fc3244b119e21e1191e8ba8517
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-21 13:57:27 +0000

    change flex-col to flexrow convesion to lg instead of md

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -31,14 +31,14 @@ export default function Accordion() {
               </button>
               <div
                 className={clsx(
-                  "text-lg w-10/12 md:w-3/4 m-auto flex flex-col md:flex-row <justify-start></justify-start> py-10 text-left gap-20",
+                  "text-lg w-10/12 md:w-3/4 m-auto flex flex-col lg:flex-row <justify-start></justify-start> py-10 text-left gap-20",
                   {
                     block: openAccordionSection === index,
                     hidden: openAccordionSection !== index,
                   },
                 )}
               >
-                <div className="md:w-1/2 text-pretty flex flex-col gap-5">
+                <div className="lg:w-1/2 text-pretty flex flex-col gap-5">
                   <p className="mb-5">{section.content}</p>
                   <h3 className="text-2xl">{section.secondaryTitle}</h3>
                   <p>{section.secondaryContent}</p>
