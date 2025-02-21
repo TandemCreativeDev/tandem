5e65f1a07337be05a29cd37553b51255b7f105e1
maxitect <maxime.downe@gmail.com>
2025-02-21 13:59:24 +0000

    refactor: implement centrally stored contact element data to declutter Contact.tsx and provide more centralised controls

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,18 +6,20 @@ import { toast } from "react-hot-toast";
 import TextInput from "./ui/TextInput";
 import Checkbox from "./ui/Checkbox";
 
-export default function ContactSection() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    company: "",
-    email: "",
-    phoneNumber: "",
-    country: "",
-    message: "",
-  });
+import contact_form from "@/data/contact_form.json";
 
-  const handleChange = (e: { target: { name: string; value: unknown } }) => {
+export default function ContactSection() {
+  const contactForm = contact_form.reduce<{ [key: string]: string }>(
+    (acc, field) => {
+      acc[field.id] = "";
+      return acc;
+    },
+    {}
+  );
+
+  const [formData, setFormData] = useState(contactForm);
+
+  const handleChange = (e: { target: { name: string; value: string } }) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
@@ -25,12 +27,9 @@ export default function ContactSection() {
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
     const fd = new FormData();
-    fd.append("firstName", formData.firstName);
-    fd.append("lastName", formData.lastName);
-    fd.append("company", formData.company);
-    fd.append("email", formData.email);
-    fd.append("phoneNumber", formData.phoneNumber);
-    fd.append("message", formData.message);
+    for (const key in formData) {
+      fd.append(key, formData[key]);
+    }
     const toastId = toast.loading("Sending message...");
     try {
       const response = await fetch("/api/contact", {
@@ -68,63 +67,20 @@ export default function ContactSection() {
             <span className="text-black">Send us a message</span> we&apos;d love
             to hear from you!
           </legend>
-          <TextInput
-            label={"First Name"}
-            id={"firstName"}
-            name={"firstName"}
-            type={"text"}
-            value={formData.firstName}
-            onChange={handleChange}
-            required
-          />
-          <TextInput
-            label={"Last Name"}
-            id={"lastName"}
-            name={"lastName"}
-            type={"text"}
-            value={formData.lastName}
-            onChange={handleChange}
-            required
-          />
-          <TextInput
-            label={"Company"}
-            id={"company"}
-            name={"company"}
-            type={"text"}
-            value={formData.company}
-            onChange={handleChange}
-            className="sm:col-span-2"
-          />
-          <TextInput
-            label={"Email"}
-            id={"email"}
-            name={"email"}
-            type={"email"}
-            value={formData.email}
-            onChange={handleChange}
-            className="sm:col-span-2"
-            required
-          />
-          <TextInput
-            label={"Phone Number"}
-            id={"phoneNumber"}
-            name={"phoneNumber"}
-            type={"tel"}
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            className="sm:col-span-2"
-          />
-          <TextInput
-            label={"Message"}
-            id={"message"}
-            name={"message"}
-            type={"text"}
-            value={formData.message}
-            onChange={handleChange}
-            className="sm:col-span-2"
-            required
-            long
-          />
+          {contact_form.map((field) => (
+            <TextInput
+              key={field.id}
+              label={field.label}
+              id={field.id}
+              name={field.name}
+              type={field.type}
+              value={formData[field.id]}
+              onChange={handleChange}
+              required={field.required}
+              long={field.long}
+              className={field.className}
+            />
+          ))}
           <Checkbox
             id={"privacyPolicy"}
             name={"privacyPolicy"}
