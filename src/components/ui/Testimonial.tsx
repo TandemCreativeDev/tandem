84bc11cfb5c89b3b9d4ba1279ecffdaa9dc1e7a4
maxitect <maxime.downe@gmail.com>
2025-02-24 16:03:43 +0000

    fix: image cutting out for wedgy's testimonial in Testimonial.tsx

--- src/components/ui/Testimonial.tsx
+++ src/components/ui/Testimonial.tsx
@@ -14,26 +14,22 @@ export default function Testimonial({
   src,
 }: TestimonialProps) {
   return (
-    <>
-      <div className="flex flex-col ">
-        <p className="">{content}</p>
-        <div className="flex mt-5 border-t-2 border-t-gray-500 pt-4">
-          <div className="overflow-hidden h-10 w-10 rounded-full mr-5">
-            <Image
-              src={`/client-pics/client-${src}.jpeg`}
-              alt={`Portrait of ${client}`}
-              width={40}
-              height={40}
-            ></Image>
-          </div>
-          <div className="flex flex-col font-tandem-mono-medium uppercase">
-            <p className="text-sm xl:text-base">{client}</p>
-            <p className="text-gray-500 text-sm xl:text-base">
-              {clientPosition}
-            </p>
-          </div>
+    <div className="flex flex-col">
+      <p>{content}</p>
+      <div className="flex mt-5 border-t-2 border-t-gray-500 pt-4">
+        <div className="relative h-10 w-10 overflow-hidden rounded-full mr-5">
+          <Image
+            src={`/clients/${src}.jpeg`}
+            alt={`Portrait of ${client}`}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="flex flex-col font-tandem-mono-medium uppercase">
+          <p className="text-sm xl:text-base">{client}</p>
+          <p className="text-gray-500 text-sm xl:text-base">{clientPosition}</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
