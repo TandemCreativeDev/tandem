b27e0fcc4b7d105ca1f8972996d5fa3fd39b0620
maxitect <maxime.downe@gmail.com>
2025-02-24 15:52:32 +0000

    chore: add structured heading hierarchy in Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,11 +7,11 @@ export default function FooterSection() {
       id="footer"
       className="flex grid-cols-12 flex-col border-t-2 border-t-gray-700 bg-black py-10 font-tandem-mono-medium uppercase text-white md:grid"
     >
-      <span className="col-span-2 col-start-1 mb-10 pl-20 md:mb-0 md:pl-10">
+      <h2 className="col-span-2 col-start-1 mb-10 pl-20 md:mb-0 md:pl-10">
         tandem creative dev
-      </span>
+      </h2>
       <div className="col-span-1 col-start-8 h-full pl-20 md:pl-0">
-        <h2 className="mb-5 md:mb-20">index</h2>
+        <h3 className="mb-5 md:mb-20">index</h3>
         <div className="flex flex-col font-tandem-condensed-medium text-5xl">
           <Link href={"#home"}>Home</Link>
           <Link href={"#about"}>About</Link>
@@ -24,12 +24,12 @@ export default function FooterSection() {
       <div className="col-span-12 col-start-1 flex py-10">
         <div className="flex w-7/12 flex-col gap-32 pl-10 md:flex-row">
           <div>
-            <h3 className="text-gray-500">timezone</h3>
+            <h3 className="text-gray-400">timezone</h3>
             <Time />
           </div>
         </div>
         <div className="w-5/12">
-          <h3 className="text-gray-500">socials</h3>
+          <h3 className="text-gray-400">social</h3>
           <div className="flex flex-col gap-4 md:flex-row">
             <Link
               target="_blank"
