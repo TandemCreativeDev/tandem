4a6a55a8a1be474a88654cf99c4a124617abd78e
maxitect <maxime.downe@gmail.com>
2025-02-24 23:24:27 +0000

    chore: add animation for hamburger icon and keep h1 home link on mobile nav menu in Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,16 @@
 import { useState, useEffect } from "react";
 import Nav from "./Nav";
 import Time from "../ui/Time";
-import { BsList, BsX } from "react-icons/bs";
 import Link from "next/link";
+import clsx from "clsx";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [hasScrolledPastHero, setHasScrolledPastHero] = useState(false);
+
+  const hamburgerClasses =
+    "h-[2.5px] w-full transition-all motion-reduce:transition-none duration-500 ease-in-out";
+  const bunClasses = "rotate-45 bg-black";
 
   useEffect(() => {
     const handleScroll = () => {
@@ -55,23 +59,44 @@ export default function Header() {
         <div className="w-1/3 xl:block hidden">
           <Time />
         </div>
-        <Link className="w-full xl:w-1/3" href={"#home"}>
+        <Link
+          className={clsx(
+            "w-full xl:w-1/3 justify-center z-10",
+            isOpen ? "text-black" : "text-white"
+          )}
+          href={"#home"}
+        >
           <h1 className="text-center">tandem creative dev</h1>
         </Link>
         <div className="w-1/3 self-end xl:block hidden">
           <Nav setIsOpen={setIsOpen} />
         </div>
-        <BsList
-          size={40}
-          className="xl:hidden absolute top-3 right-5"
+        <button
+          className="mr-2 lg:hidden flex flex-col justify-between w-8 h-6 cursor-pointer z-50 absolute right-5 top-5"
           onClick={() => setIsOpen(!isOpen)}
-        />
+          aria-label="Toggle navigation"
+        >
+          <div
+            className={clsx(
+              hamburgerClasses,
+              isOpen ? `${bunClasses} translate-y-[10px]` : "bg-white"
+            )}
+          ></div>
+          <div
+            className={clsx(
+              hamburgerClasses,
+              isOpen ? "opacity-0" : "bg-white"
+            )}
+          ></div>
+          <div
+            className={clsx(
+              hamburgerClasses,
+              isOpen ? `-${bunClasses} -translate-y-[12px]` : "bg-white"
+            )}
+          ></div>
+        </button>
         {isOpen === true ? (
           <div className="absolute h-screen top-0 left-0 w-screen bg-white flex justify-center items-center text-5xl overflow-hidden">
-            <BsX
-              onClick={() => setIsOpen(false)}
-              className="absolute right-4 top-2 text-black"
-            />
             <Nav setIsOpen={setIsOpen} />
           </div>
         ) : undefined}
