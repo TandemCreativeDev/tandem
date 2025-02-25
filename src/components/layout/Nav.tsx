859c60e988d690546b429a77470ab6a9e80532e3
maxitect <maxime.downe@gmail.com>
2025-02-25 00:05:42 +0000

    chore: make Nav.tsx an unstructured list and add animation hover animations

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -15,14 +15,28 @@ export default function Nav({ setIsOpen }: NavProps) {
   ];
 
   return (
-    <nav
-      className={`flex xl:flex-row flex-col justify-end gap-5 text-black xl:text-white`}
-    >
-      {items.map((item, index) => (
-        <Link onClick={() => setIsOpen(false)} key={index} href={`#${item}`}>
-          {item}
-        </Link>
-      ))}
+    <nav className="justify-end gap-5 text-black xl:text-white">
+      <ul className="flex flex-col gap-6 group md:flex-row">
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className="relative list-none transition-all motion-reduce:transition-none duration-500 group-hover:text-gray-500 hover:!text-black"
+          >
+            <Link
+              onClick={() => setIsOpen(false)}
+              href={`#${item}`}
+              className="
+                before:absolute before:top-0 before:left-0 before:w-full before:h-full
+                before:bg-white before:z-[-1] before:transition-transform motion-reduce:before:transition-none before:duration-500
+                before:origin-right before:scale-x-0
+                hover:before:origin-left hover:before:scale-x-100
+              "
+            >
+              {item}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
