fb690ee3b3d6f638cfbe3818b5180d05236e7b9a
maxitect <maxime.downe@gmail.com>
2025-02-25 12:06:26 +0000

    refactor: apply section link component in Nav.tsx

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,42 +1,16 @@
-import Link from "next/link";
+import SectionLinks from "../ui/SectionLinks";
 
 interface NavProps {
   setIsOpen: (arg: boolean) => void;
 }
 
 export default function Nav({ setIsOpen }: NavProps) {
-  const items = [
-    "about",
-    "services",
-    "work",
-    "testimonials",
-    "team",
-    "contact",
-  ];
-
   return (
     <nav className="justify-end gap-5 text-black xl:text-white">
-      <ul className="flex flex-col gap-6 group xl:flex-row">
-        {items.map((item, index) => (
-          <li
-            key={index}
-            className="relative list-none transition-all motion-reduce:transition-none duration-500 group-hover:text-gray-500 hover:!text-black"
-          >
-            <Link
-              onClick={() => setIsOpen(false)}
-              href={`#${item}`}
-              className="
-                before:absolute before:top-0 before:left-0 before:w-full before:h-full
-                before:bg-white before:z-[-1] before:transition-transform motion-reduce:before:transition-none before:duration-500
-                before:origin-right before:scale-x-0
-                hover:before:origin-left hover:before:scale-x-100
-              "
-            >
-              {item}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <SectionLinks
+        className="flex flex-col gap-6 xl:flex-row"
+        onClick={() => setIsOpen(false)}
+      />
     </nav>
   );
 }
