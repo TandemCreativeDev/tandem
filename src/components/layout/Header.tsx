ef1ae00422a316f9f6e1a14ed9468d6edfe6cafc
maxitect <maxime.downe@gmail.com>
2025-02-25 10:43:00 +0000

    refactor: import burger into Header.tsx and show time starting from medium size

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,14 +4,11 @@ import Nav from "./Nav";
 import Time from "../ui/Time";
 import Link from "next/link";
 import clsx from "clsx";
+import Burger from "../ui/Burger";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [hasScrolledPastHero, setHasScrolledPastHero] = useState(false);
-
-  const hamburgerClasses =
-    "h-[2.5px] w-full transition-all motion-reduce:transition-none duration-500 ease-in-out";
-  const bunClasses = "rotate-45 bg-black";
 
   useEffect(() => {
     const handleScroll = () => {
@@ -55,8 +52,8 @@ export default function Header() {
         z-50
       `}
     >
-      <div className="m-auto w-11/12 flex xl:justify-between items-center justify-center">
-        <div className="xl:block hidden">
+      <div className="m-auto w-11/12 flex justify-between items-center h-6">
+        <div className="md:block hidden">
           <Time />
         </div>
         <Link
@@ -71,30 +68,7 @@ export default function Header() {
         <div className="self-end xl:block hidden">
           <Nav setIsOpen={setIsOpen} />
         </div>
-        <button
-          className="mr-2 lg:hidden flex flex-col justify-between w-8 h-6 cursor-pointer z-50 absolute right-5 top-5"
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Toggle navigation"
-        >
-          <div
-            className={clsx(
-              hamburgerClasses,
-              isOpen ? `${bunClasses} translate-y-[10px]` : "bg-white"
-            )}
-          ></div>
-          <div
-            className={clsx(
-              hamburgerClasses,
-              isOpen ? "opacity-0" : "bg-white"
-            )}
-          ></div>
-          <div
-            className={clsx(
-              hamburgerClasses,
-              isOpen ? `-${bunClasses} -translate-y-[12px]` : "bg-white"
-            )}
-          ></div>
-        </button>
+        <Burger isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
         {isOpen === true ? (
           <div className="absolute h-screen top-0 left-0 w-screen bg-white flex justify-center items-center text-5xl overflow-hidden">
             <Nav setIsOpen={setIsOpen} />
