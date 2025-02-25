4e9e53a17523cdda9db6b78cb047547016e507ed
maxitect <maxime.downe@gmail.com>
2025-02-25 12:06:57 +0000

    refactor: apply section link component in Footer.tsx to make an unstructured list with hovers

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Time from "../ui/Time";
+import SectionLinks from "../ui/SectionLinks";
 
 export default function FooterSection() {
   return (
@@ -10,16 +11,12 @@ export default function FooterSection() {
       <h2 className="col-span-2 col-start-1 mb-10 pl-20 md:mb-0 md:pl-10">
         tandem creative dev
       </h2>
-      <div className="col-span-1 col-start-8 h-full pl-20 md:pl-0">
+      <div className="col-span-2 col-start-8 h-full pl-20 md:pl-0">
         <h3 className="mb-5 md:mb-20">index</h3>
-        <div className="flex flex-col font-tandem-condensed-medium text-5xl">
-          <Link href={"#home"}>Home</Link>
-          <Link href={"#about"}>About</Link>
-          <Link href={"#services"}>Services</Link>
-          <Link href={"#work"}>Work</Link>
-          <Link href={"#testimonials"}>Testimonials</Link>
-          <Link href={"#team"}>Team</Link>
-        </div>
+        <SectionLinks
+          className="flex flex-col font-tandem-condensed-medium text-5xl"
+          startIndex={0}
+        />
       </div>
       <div className="col-span-12 col-start-1 flex py-10">
         <div className="flex w-7/12 flex-col gap-32 pl-10 md:flex-row">
@@ -30,7 +27,7 @@ export default function FooterSection() {
         </div>
         <div className="w-5/12">
           <h3 className="text-gray-400">social</h3>
-          <div className="flex flex-col gap-4 md:flex-row">
+          <div className="flex flex-col gap-4 text-white">
             <Link
               target="_blank"
               href={"https://www.linkedin.com/company/tandemcreativedev/"}
