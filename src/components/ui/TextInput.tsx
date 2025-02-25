8801817b95a1d9eec69f71c63becaba5b3a831dd
maxitect <maxime.downe@gmail.com>
2025-02-25 17:58:57 +0000

    fix: correct order in twMerge and class prioritisation in TextInput.tsx

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -32,10 +32,10 @@ export default function TextInput({
   const baseClasses =
     "block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm/6 mt-2.5 ";
   return (
-    <div className={twMerge(className, "col-span-2")}>
+    <div className={twMerge("col-span-2 xl:col-span-1", className)}>
       <label
         htmlFor={id}
-        className={twMerge(labelClass, "block text-sm/6 font-semibold")}
+        className={twMerge("block text-sm/6 font-semibold", labelClass)}
       >
         {label}
         {required ? (
@@ -52,7 +52,7 @@ export default function TextInput({
           rows={4}
           value={value}
           onChange={onChange}
-          className={twMerge(inputClass, baseClasses)}
+          className={twMerge(baseClasses, inputClass)}
           required={required}
         />
       ) : (
@@ -62,7 +62,7 @@ export default function TextInput({
           type={type}
           value={value}
           onChange={onChange}
-          className={twMerge(inputClass, baseClasses)}
+          className={twMerge(baseClasses, inputClass)}
           required={required}
         />
       )}
