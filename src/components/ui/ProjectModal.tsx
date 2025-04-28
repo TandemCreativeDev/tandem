b26eac132634850e92bad915a2d95c26b03f5488
maxitect <maxime.downe@gmail.com>
2025-04-28 15:17:13 +0100

    fix: update ProjectModal.tsx for mobile mode

--- src/components/ui/ProjectModal.tsx
+++ src/components/ui/ProjectModal.tsx
@@ -48,17 +48,20 @@ export default function ProjectModal({
       />
 
       <div className="relative bg-zinc-950/90 rounded-xl overflow-hidden w-11/12 md:w-9/12 lg:w-7/12 max-h-[85vh] p-8 shadow-glow shadow-zinc-900">
-        <button
-          className="absolute top-6 right-6 w-10 h-10 flex items-center justify-center rounded-full bg-transparent border-[0.75px] text-white hover:bg-white hover:text-zinc-950/90"
-          onClick={onClose}
-          aria-label="Close modal"
-        >
-          <span className="text-lg">X</span>
-        </button>
-
-        <h1 className="text-white text-3xl uppercase mb-8 font-tandem-condensed-medium">
-          {title}
-        </h1>
+        <div className="flex justify-between">
+          <h2 className="text-white text-3xl uppercase mb-8 font-tandem-condensed-medium truncate">
+            {title}
+          </h2>
+          <div className="flex-shrink-0">
+            <button
+              className="flex-shrink-0 w-10 h-10 flex items-center justify-center rounded-full bg-transparent border-[0.75px] text-white hover:bg-white hover:text-zinc-950/90"
+              onClick={onClose}
+              aria-label="Close modal"
+            >
+              <span className="text-lg">X</span>
+            </button>
+          </div>
+        </div>
 
         <p className="text-zinc-300 mb-6 text-lg">{description}</p>
 
@@ -72,7 +75,12 @@ export default function ProjectModal({
         </div>
 
         {websiteUrl && (
-          <UrlButton label="Visit Website" url={websiteUrl} project={title} />
+          <UrlButton
+            label="Visit Website"
+            url={websiteUrl}
+            project={title}
+            className="mb-4 md:mb-0"
+          />
         )}
         {githubUrl && (
           <UrlButton
