183f60d7b0c62bb1570ebaedf2fd80a53a66726a
maxitect <maxime.downe@gmail.com>
2025-07-29 18:18:54 +0100

    chore: update MobileProjectImages.tsx to read jpg and render on mobile option

--- src/components/ui/MobileProjectImages.tsx
+++ src/components/ui/MobileProjectImages.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import projects from "@/data/projects.json";
+
 interface MobileProjectImagesProps {
   selectedProject: number;
   openModal: () => void;
@@ -14,23 +15,37 @@ export default function MobileProjectImages({
   return (
     <div className="md:hidden text-white flex flex-col justify-center items-center">
       {projects.map((project, index) => (
-        <div key={index} className="pt-10">
-          <h3 className="uppercase text-2xl font-tandem-medium mb-2">
-            {project.title}
-          </h3>
-          <Image
-            priority
-            sizes="100vw"
-            height={300}
-            width={380}
-            alt={`Mockup of ${project.title}`}
-            src={`/mobile/${project.src}.png`}
-            onClick={() => {
-              setSelectedProject(index);
-              openModal();
-            }}
-            className="cursor-pointer"
-          />
+        <div key={index} className="pt-10 w-full">
+          {project.renderOnMobile ? (
+            <h3 className="uppercase text-2xl font-tandem-medium mb-2">
+              {project.title}
+            </h3>
+          ) : (
+            <button
+              onClick={() => {
+                setSelectedProject(index);
+                openModal();
+              }}
+              className="uppercase text-2xl font-tandem-medium mb-2 text-left hover:text-gray-300 transition-colors"
+            >
+              {project.title}
+            </button>
+          )}
+          {project.renderOnMobile && (
+            <Image
+              priority
+              sizes="100vw"
+              height={300}
+              width={380}
+              alt={`Mockup of ${project.title}`}
+              src={`/mobile/${project.src}.jpg`}
+              onClick={() => {
+                setSelectedProject(index);
+                openModal();
+              }}
+              className="cursor-pointer"
+            />
+          )}
         </div>
       ))}
     </div>
