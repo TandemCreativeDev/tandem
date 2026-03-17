d2413bed0674cbb3561c3a929d5d842adbaeb78c
maxitect <maxime.downe@gmail.com>
2026-03-17 10:10:49 +0000

    style(clients): regularise padding

--- src/components/sections/Clients.tsx
+++ src/components/sections/Clients.tsx
@@ -2,7 +2,7 @@ import clients from "@/data/clients.json";
 
 export default function ClientsSection() {
   return (
-    <section className="px-10 pt-10 pb-36">
+    <section className="p-10">
       <h2 className="sr-only">Organisations we&apos;ve worked with</h2>
       <p className="font-tandem-condensed-medium text-4xl uppercase leading-tight text-black md:text-6xl lg:text-7xl">
         {clients.map((client, i) => (
