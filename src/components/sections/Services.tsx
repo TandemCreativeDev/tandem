f8066d9ef050c66e54d0a83d2f17a7f40a44db75
maxitect <maxime.downe@gmail.com>
2026-03-18 22:38:59 +0000

    fix(nav): sections order for nav

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -4,8 +4,8 @@ import nav_items from "@/data/nav_items.json";
 
 export default function ServicesSection() {
   return (
-    <section id={nav_items[2]}>
-      <h2 className="sr-only">{nav_items[2]}</h2>
+    <section id={nav_items[3]}>
+      <h2 className="sr-only">{nav_items[3]}</h2>
       <Accordion />
     </section>
   );
